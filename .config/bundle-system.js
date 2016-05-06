d024ebf626010f0717f45164613fd263bafe8873
Miguel Couto <jmiguelcouto@gmail.com>
2016-05-06 15:03:27 +0100

    remove unneeded packages.

--- .config/bundle-system.js
+++ .config/bundle-system.js
@@ -37,16 +37,9 @@ function getSystemJsBundleConfig(cb) {
     'rxjs':                       { defaultExtension: 'js' },
   };
   var packageNames = [
-    '@angular/common',
-    '@angular/compiler',
     '@angular/core',
-    '@angular/http',
-    '@angular/platform-browser',
+    '@angular/common',
     '@angular/platform-browser-dynamic',
-    '@angular/router',
-    '@angular/router-deprecated',
-    '@angular/testing',
-    '@angular/upgrade',
   ];
   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
   packageNames.forEach(function(pkgName) {
